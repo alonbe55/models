e972304a59bdaa34238df903c7c8d8fb0219007d
Alon <alonbe55@gmail.com>
2019-08-05 21:11:48 +0300

    only graph v/s

--- modeling_motor.py
+++ modeling_motor.py
@@ -1,23 +1,25 @@
 import matplotlib.pyplot as plt
 
-current_time = 0.0
 dt = 0.01
-vector = [0.0,0.0] #[angle, angular velocity]
+vector = [0.0,0.0,0.0] #[angle, angular velocity,current_time]
 w_prev = 0.0
 
-def iteration(voltage=12, torque=0):
-    global current_time, dt, vector, w_prev
-    vector[1] = (46.513*voltage) - (232.565*torque)
+def iteration(vector, voltage=12):
+    torque = (0.2*voltage) - (0.0043*vector[1])
+    a = ((0.2*voltage) - (0.0043*vector[1])) / 0.00007753
+    vector[1] = vector[1] + (a*dt)
+    #vector[1] = (46.513*voltage) - (232.565*torque)
     #vector[0] = vector[0] + (((vector[1]+w_prev)*dt)/2)
     #a = (vector[1]-w_prev)/dt
-    plt.plot(current_time, vector[1], 'bo')
+    plt.plot(vector[2], vector[1], 'bo')
     plt.draw()
     #plt.scatter(current_time,vector[1],s=100)
     plt.pause(0.0001)
     print(vector[1])
     #plt.clf()
-    current_time = current_time + dt
+    vector[2] = vector[2] + dt
     w_prev = vector[1]
+    return vector
 
 while True:
-    iteration()
+    vector = iteration(vector)
